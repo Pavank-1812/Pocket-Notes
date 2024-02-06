ccbc1c903cebdbb2923eb42428195fb2acc6e2b8
Pavank-1812 <148217455+Pavank-1812@users.noreply.github.com>
2024-02-06 23:26:25 +0530

    updated css for Popup

---
+++ src/components/NotesSection/NotesSection.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function NotesSection() {
+    return (
+        <>
+            <div>
+                
+            </div>
+        </>
+    )
+}
+
+export default NotesSection
